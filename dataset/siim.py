3c63d74fb70fdbd0c16eace5788ec201f5b3c1e8
Xie Di <xiedidan@yeah.net>
2019-07-01 23:57:35 +0800

    maskrcnn with multiple masks in one sample

--- dataset/siim.py
+++ dataset/siim.py
@@ -17,68 +17,86 @@ import pandas as pd
 from tqdm import tqdm
 from torchvision import transforms
 import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 from util import mask2rle, rle2mask
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class SIIM_MaskRCNN_Dataset(torch.utils.data.Dataset):
-    def __init__(self, df_path, img_dir):
+    def __init__(self, df_path, img_dir, lst_dir, fold=0, phrase='train'):
         self.df = pd.read_csv(df_path)
         self.height = 256
         self.width = 256
         self.image_dir = img_dir
         self.image_info = collections.defaultdict(dict)
-
-        self.df = self.df.drop_duplicates('ImageId', keep='last').reset_index(drop=True)
-
-        counter = 0
+        
+        # TODO : combine multiple masks in the same sample
+        
+        # self.df = self.df.drop_duplicates('ImageId', keep='last').reset_index(drop=True)
+        
         for index, row in tqdm(self.df.iterrows(), total=len(self.df)):
             image_id = row['ImageId']
-            image_path = os.path.join(self.image_dir, image_id)
-            if os.path.exists(image_path + '.png') and row[" EncodedPixels"].strip() != '-1':
-                self.image_info[counter]["image_id"] = image_id
-                self.image_info[counter]["image_path"] = image_path
-                self.image_info[counter]["annotations"] = row[" EncodedPixels"].strip()
-                counter += 1
+            
+            if image_id in self.image_info.keys():
+                # append data to image_info dict
+                self.image_info[image_id]['annotations'].append(row[' EncodedPixels'].strip())
+            else:
+                image_path = os.path.join(self.image_dir, image_id)
+                
+                if os.path.exists(image_path + '.png') and row[" EncodedPixels"].strip() != '-1':
+                    self.image_info[image_id]["image_path"] = image_path
+                    self.image_info[image_id]["annotations"] = [row[" EncodedPixels"].strip()]
+
+        list_file = os.path.join(lst_dir, '{}-{}.csv'.format(phrase, fold))
+        list_pd = pd.read_csv(list_file)
+
+        self.sample_list = list(list_pd['ImageId'])
 
     def __getitem__(self, idx):
-        img_path = self.image_info[idx]["image_path"]
+        image_id = self.sample_list[idx]
+        info = self.image_info[image_id]
+
+        img_path = info["image_path"]
         img = Image.open(img_path + '.png').convert("RGB")
         width, height = img.size
         img = img.resize((self.width, self.height), resample=Image.BILINEAR)
-        info = self.image_info[idx]
+        
+        boxes = []
+        masks = []
 
-        mask = rle2mask(info['annotations'], width, height)
-        mask = Image.fromarray(mask)
-        mask = mask.resize((self.width, self.height), resample=Image.BILINEAR)
-        mask = np.expand_dims(mask, axis=0)
+        for anno in info['annotations']:
+            mask = rle2mask(anno, width, height)
+            mask = Image.fromarray(mask)
+            mask = mask.resize((self.width, self.height), resample=Image.BILINEAR)
+            mask = np.expand_dims(mask, axis=0)
 
-        pos = np.where(np.array(mask)[0, :, :])
-        xmin = np.min(pos[1])
-        xmax = np.max(pos[1])
-        ymin = np.min(pos[0])
-        ymax = np.max(pos[0])
+            pos = np.where(np.array(mask)[0, :, :])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
 
-        boxes = torch.as_tensor([[xmin, ymin, xmax, ymax]], dtype=torch.float32)
-        labels = torch.ones((1,), dtype=torch.int64)
-        masks = torch.as_tensor(mask, dtype=torch.float32)
+            box = [xmin, ymin, xmax, ymax]
 
+            boxes.append(box)
+            masks.append(mask.squeeze())
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        masks = torch.as_tensor(np.stack(masks, axis=0), dtype=torch.float32)
+        areas = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        labels = torch.ones((len(areas),), dtype=torch.int64)
         image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        iscrowd = torch.zeros((1,), dtype=torch.int64)
-
+        iscrowd = torch.zeros((len(areas),), dtype=torch.int64)
+        
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
         target["masks"] = masks / 255
         target["image_id"] = image_id
-        target["area"] = area
+        target["area"] = areas
         target["iscrowd"] = iscrowd
 
         return transforms.ToTensor()(img), target
 
     def __len__(self):
-        return len(self.image_info)
+        return len(self.sample_list)
